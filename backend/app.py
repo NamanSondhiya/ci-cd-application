d43589ca3774547b5f748f6bed79d721c45b0dd9
Naman-UbU <namansondhiya17@gmail.com>
2025-08-06 15:38:00 +0530

    CORSv1

--- backend/app.py
+++ backend/app.py
@@ -3,15 +3,19 @@ from data_example import Tools
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins='*') 
 
 @app.route('/')
 def index():
-    return "hello from backend"
+    return "Greeting's From the BACKEND"
 
 @app.route('/jet')
 def jet():
-    return jsonify(Tools)
+    response = jsonify(Tools)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
