1c672df789afc10e8df4d2eb65543da677559da2
Naman-UbU <namansondhiya17@gmail.com>
2025-08-06 15:45:57 +0530

    CORS resolved

--- backend/app.py
+++ backend/app.py
@@ -12,10 +12,6 @@ def index():
 @app.route('/jet')
 def jet():
     response = jsonify(Tools)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
